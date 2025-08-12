fe0370384b94532bdb21738c988d738eb7160c02
Ahuva <ahuvael02@gmail.com>
2025-08-12 21:03:15 +0300

    rename folder

--- src/components/pages/home-page/HomePage.component.tsx
+++ src/components/pages/home-page/HomePage.component.tsx
@@ -1,7 +1,7 @@
 import ButtonSection from "../../button-section/ButtonSection.component";
 import { AlertsTable } from "../../tabels/alert-table/AlertTable.component";
 import { ArrayAlertsTable } from "../../tabels/array-alerts/ArrayAlerts.component";
-import { RequestsTable } from "../../tabels/requestTable/RequestTable.component";
+import { RequestsTable } from "../../tabels/request-table/RequestTable.component";
 import "./HomePage.css";
 
 const HomePage: React.FC = () => {
